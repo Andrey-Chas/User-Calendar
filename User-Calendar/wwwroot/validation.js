8d4869fefc812972081bb58bf6d7d3a2a34b2736
Andrey Chasovskoy <andrchas777@gmail.com>
2023-06-23 18:10:53 +0300

    JASONDataUpdated

--- User-Calendar/wwwroot/validation.js
+++ User-Calendar/wwwroot/validation.js
@@ -36,18 +36,77 @@ const title = document.getElementById("title");
 const date = document.getElementById("date");
 const organizer = document.getElementById("organizer");
 const link = document.getElementById("link");
+const calendarCells = document.querySelectorAll(".text-bg-info");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+var dayToGet = d.getDay();
+var count = 0;
+var totalDays = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
+var startDay = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dates = [];
 
-const dataToFetch = {
-    "summary": "supetest",
-    "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
-    "start": {
-        "dateTime": "2023-06-17T14:00:00"
+for (var i = 0; i < 7; i++) {
+    if (startDay > totalDays) {
+        startDay = 1;
+    }
+    if (days[dayToGet + count] == "Saturday") {
+        document.getElementById("d" + (i + 1)).innerHTML = days[dayToGet + count] + " " + startDay;
+        dates.push(startDay);
+        startDay++;
+        count = 0;
+        dayToGet = 0;
+    }
+    else {
+        document.getElementById("d" + (i + 1)).innerHTML = days[dayToGet + count] + " " + startDay;
+        dates.push(startDay);
+        count++;
+        startDay++;
+    }
+}
+
+function displayDataFromCalendar() {
+    document.getElementById("dataToDisplay").innerHTML = "Data";
+}
+
+for (var calendarCell of calendarCells) {
+    calendarCell.addEventListener("click", displayDataFromCalendar);
+}
+
+const dataToFetch = [
+    {
+        "summary": "supetest",
+        "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
+        "start": {
+            "dateTime": "2023-06-27T14:00:00"
+        },
+        "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
+        "organizer": {
+            "email": "outlook_556C818D2795D20F@outlook.com"
+        },
+        "link": "https://link.com"
     },
-    "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
-    "organizer": {
-        "email": "outlook_556C818D2795D20F@outlook.com"
-    },
-    "link": "https://link.com"
+    {
+        "summary": "supetest1",
+        "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
+        "start": {
+            "dateTime": "2023-06-29T14:00:00"
+        },
+        "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
+        "organizer": {
+            "email": "outlook_556C818D2795D20F@outlook.com"
+        },
+        "link": "https://link.com"
+    }
+]
+
+for (var i = 0; i < dataToFetch.length; i++) {
+    for (var j = 0; j < dates.length; j++) {
+        if (new Date(dataToFetch[i].start.dateTime).getDate() == dates[j]) {
+            document.getElementById("p" + (j + 1)).innerHTML = dataToFetch[i].summary;
+        }
+    }
 }
 
 const data = JSON.stringify(dataToFetch);
@@ -97,11 +156,11 @@ form.addEventListener("submit", function (event) {
 function displayData() {
     var text = localStorage.getItem("JSONData");
     var neededData = JSON.parse(text);
-    title.innerHTML = "Title: " + neededData.summary;
-    date.innerHTML = "Date: " + neededData.start.dateTime;
-    attendees.innerHTML = "Attendees: " + neededData.attendees;
-    organizer.innerHTML = "Organizer: " + neededData.organizer.email;
-    link.innerHTML = "Link: " + neededData.link;
+    title.innerHTML = "Title: " + neededData[0].summary;
+    date.innerHTML = "Date: " + neededData[0].start.dateTime;
+    attendees.innerHTML = "Attendees: " + neededData[0].attendees;
+    organizer.innerHTML = "Organizer: " + neededData[0].organizer.email;
+    link.innerHTML = "Link: " + neededData[0].link;
 }
 
 //function fetchData() {

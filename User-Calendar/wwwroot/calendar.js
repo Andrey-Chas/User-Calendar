5b4b7f16ea877f5963c02621a4e53a17f44f17e9
Andrey Chasovskoy <andrchas777@gmail.com>
2023-06-25 15:19:12 +0300

    JSONDisplayDataUpdated

---
+++ User-Calendar/wwwroot/calendar.js
@@ -0,0 +1,83 @@
+﻿const title = document.getElementById("title");
+const date = document.getElementById("date");
+const organizer = document.getElementById("organizer");
+const link = document.getElementById("link");
+const calendarCells = document.querySelectorAll(".text-bg-info");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+const dataToFetch = [
+    {
+        "summary": "supetest",
+        "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
+        "start": {
+            "dateTime": "2023-06-27T14:00:00"
+        },
+        "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
+        "organizer": {
+            "email": "outlook_556C818D2795D20F@outlook.com"
+        },
+        "link": "https://link.com"
+    },
+    {
+        "summary": "supetest1",
+        "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
+        "start": {
+            "dateTime": "2023-06-29T14:00:00"
+        },
+        "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
+        "organizer": {
+            "email": "outlook_556C818D2795D20F@outlook.com"
+        },
+        "link": "https://link.com"
+    }
+]
+var dayToGet = d.getDay();
+var count = 0;
+var totalDays = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
+var startDay = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dates = [];
+
+for (var i = 0; i < 7; i++) {
+    if (startDay > totalDays) {
+        startDay = 1;
+    }
+    if (days[dayToGet + count] == "Saturday") {
+        document.getElementById("d" + (i + 1)).innerHTML = days[dayToGet + count] + " " + startDay;
+        dates.push(startDay);
+        startDay++;
+        count = 0;
+        dayToGet = 0;
+    }
+    else {
+        document.getElementById("d" + (i + 1)).innerHTML = days[dayToGet + count] + " " + startDay;
+        dates.push(startDay);
+        count++;
+        startDay++;
+    }
+}
+
+function displayDataFromCalendar(event) {
+    for (var i = 0; i < dataToFetch.length; i++) {
+        if (event.target.innerHTML == dataToFetch[i].summary) {
+            document.getElementById("title").innerHTML = "Title: " + dataToFetch[i].summary;
+            document.getElementById("date").innerHTML = "Date: " + dataToFetch[i].start.dateTime;
+            document.getElementById("attendees").innerHTML = "Attendees: " + dataToFetch[i].attendees;
+            document.getElementById("organizer").innerHTML = "Organizer: " + dataToFetch[i].organizer.email;
+            document.getElementById("link").innerHTML = "Link: " + dataToFetch[i].link;
+        }
+    }
+}
+
+for (var calendarCell of calendarCells) {
+    calendarCell.addEventListener("click", displayDataFromCalendar);
+}
+
+for (var i = 0; i < dataToFetch.length; i++) {
+    for (var j = 0; j < dates.length; j++) {
+        if (new Date(dataToFetch[i].start.dateTime).getDate() == dates[j]) {
+            document.getElementById("p" + (j + 1)).innerHTML = dataToFetch[i].summary;
+        }
+    }
+}
--- User-Calendar/wwwroot/validation.js
+++ User-Calendar/wwwroot/validation.js
@@ -32,93 +32,6 @@ const formShow = document.querySelector("#showData");
 const input = document.getElementById("value");
 const label = document.getElementById("labelProvidedData");
 const error = document.querySelector(".error");
-const title = document.getElementById("title");
-const date = document.getElementById("date");
-const organizer = document.getElementById("organizer");
-const link = document.getElementById("link");
-const calendarCells = document.querySelectorAll(".text-bg-info");
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const d = new Date();
-var dayToGet = d.getDay();
-var count = 0;
-var totalDays = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
-var startDay = d.getDate();
-var month = d.getMonth();
-var year = d.getFullYear();
-var dates = [];
-
-for (var i = 0; i < 7; i++) {
-    if (startDay > totalDays) {
-        startDay = 1;
-    }
-    if (days[dayToGet + count] == "Saturday") {
-        document.getElementById("d" + (i + 1)).innerHTML = days[dayToGet + count] + " " + startDay;
-        dates.push(startDay);
-        startDay++;
-        count = 0;
-        dayToGet = 0;
-    }
-    else {
-        document.getElementById("d" + (i + 1)).innerHTML = days[dayToGet + count] + " " + startDay;
-        dates.push(startDay);
-        count++;
-        startDay++;
-    }
-}
-
-function displayDataFromCalendar(event) {
-    for (var i = 0; i < dataToFetch.length; i++) {
-        if (event.target.innerHTML == dataToFetch[i].summary) {
-            document.getElementById("title").innerHTML = "Title: " + dataToFetch[i].summary;
-            document.getElementById("date").innerHTML = "Date: " + dataToFetch[i].start.dateTime;
-            document.getElementById("attendees").innerHTML = "Attendees: " + dataToFetch[i].attendees;
-            document.getElementById("organizer").innerHTML = "Organizer: " + dataToFetch[i].organizer.email;
-            document.getElementById("link").innerHTML = "Link: " + dataToFetch[i].link;
-        }
-    }
-}
-
-for (var calendarCell of calendarCells) {
-    calendarCell.addEventListener("click", displayDataFromCalendar);
-}
-
-const dataToFetch = [
-    {
-        "summary": "supetest",
-        "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
-        "start": {
-            "dateTime": "2023-06-27T14:00:00"
-        },
-        "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
-        "organizer": {
-            "email": "outlook_556C818D2795D20F@outlook.com"
-        },
-        "link": "https://link.com"
-    },
-    {
-        "summary": "supetest1",
-        "id": "AQMkADAwATM0MDAAMS1iOTBmLTc3AGVjLTAwAi0wMAoARgAAA5qCGGg3EJFKqTdXg9Db-0UHALP8zwkk-ehHnIL88hsNTfcAAAIBDQAAALP8zwkk-ehHnIL88hsNTfcABkayNS0AAAA=",
-        "start": {
-            "dateTime": "2023-06-29T14:00:00"
-        },
-        "attendees": ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"],
-        "organizer": {
-            "email": "outlook_556C818D2795D20F@outlook.com"
-        },
-        "link": "https://link.com"
-    }
-]
-
-for (var i = 0; i < dataToFetch.length; i++) {
-    for (var j = 0; j < dates.length; j++) {
-        if (new Date(dataToFetch[i].start.dateTime).getDate() == dates[j]) {
-            document.getElementById("p" + (j + 1)).innerHTML = dataToFetch[i].summary;
-        }
-    }
-}
-
-const data = JSON.stringify(dataToFetch);
-localStorage.setItem("JSONData", data);
 
 const email = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const uuid = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
@@ -154,17 +67,19 @@ form.addEventListener("submit", function (event) {
         error.textContent = input.validationMessage;
     }
     else {
-        form.style.visibility = "collapse";
-        formShow.style.visibility = "visible";
-        displayData();
+        Redirect();
         event.preventDefault();
     }
 });
 
-function displayData() {
-    var text = localStorage.getItem("JSONData");
-    var neededData = JSON.parse(text);
+function Redirect() {
+    window.location.href = "calendar.html";
 }
+
+//function displayData() {
+//    var text = localStorage.getItem("JSONData");
+//    var neededData = JSON.parse(text);
+//}
 
 //function fetchData() {
 //    fetch("data.json")
